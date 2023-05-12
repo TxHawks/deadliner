79309883e4adae34c64524df1a5154dc568a0f7c
TxHawks <tofu.hawks@gmail.com>
2023-05-12 13:49:18 +0300

    concider recess period when calculating deadlines even if leaping over them

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -54,31 +54,32 @@ export interface TableProps {
 }
 
 export default function Table({ lastDoc, pretrial }: TableProps) {
-  const lastDocAsDate = new Date(lastDoc);
-  const pretrialAsDate = pretrial 
-    ? new Date(pretrial)
-    : undefined;
+  const lastDocAsDate = lastDoc ? new Date(lastDoc) : undefined;
+  const pretrialAsDate = pretrial ? new Date(pretrial) : undefined;
 
   const deadlines = [
-    { name: 'דיון מקדמי', date: getDeadline({ startDate: lastDocAsDate,  after: 30}), },
-    pretrialAsDate && { name: 'הודעה על דיון מקדמי', date: getDeadline({ startDate: pretrialAsDate,  before: 20}), },
-    { name: 'קביעת פגישת מהות', date: getDeadline({ startDate: lastDocAsDate,  after: 45}), },
-    { name: 'הגשת בקשות', date: getDeadline({ startDate: lastDocAsDate,  after: 53}), },
-    pretrialAsDate && { name: 'רשימת בקשות', date: getDeadline({ startDate: pretrialAsDate,  before: 20}), },
+    lastDocAsDate && { name: 'שליחת שאלונים', date: getDeadline({ startDate: lastDocAsDate,  after: 30}), },
+    lastDocAsDate && { name: 'גילוי מסמכים', date: getDeadline({ startDate: lastDocAsDate,  after: 30}), },
+    lastDocAsDate && { name: 'דיון מקדמי', date: getDeadline({ startDate: lastDocAsDate,  after: 30}), },
+    lastDocAsDate && { name: 'קביעת פגישת מהות', date: getDeadline({ startDate: lastDocAsDate,  after: 45}), },
+    lastDocAsDate && { name: 'הגשת בקשות', date: getDeadline({ startDate: lastDocAsDate,  after: 60}), },
+    lastDocAsDate && { name: 'תשובה לשאלונים', date: getDeadline({ startDate: lastDocAsDate,  after: 60}), },
+    lastDocAsDate && { name: 'עיון במסמכים', date: getDeadline({ startDate: lastDocAsDate,  after: 60}), },
+    lastDocAsDate && { name: 'גילוי ספציפי', date: getDeadline({ startDate: lastDocAsDate,  after: 60}), },
+    lastDocAsDate && { name: 'מענה לגילוי ספציפי', date: getDeadline({ startDate: lastDocAsDate,  after: 90}), },
+
     pretrialAsDate && { name: 'הודעה על בקשה לצד שני', date: getDeadline({ startDate: pretrialAsDate,  before: 27}), },
-    { name: 'שאלונים', date: getDeadline({ startDate: lastDocAsDate,  after: 30}), },
-    { name: 'תשובה לשאלונים', date: getDeadline({ startDate: lastDocAsDate,  after: 60}), },
-    { name: 'גילוי מסמכים', date: getDeadline({ startDate: lastDocAsDate,  after: 30}), },
-    { name: 'עיון במסמכים', date: getDeadline({ startDate: lastDocAsDate,  after: 60}), },
-    { name: 'גילוי ספציפי', date: getDeadline({ startDate: lastDocAsDate,  after: 60}), },
+    pretrialAsDate && { name: 'הודעה על דיון מקדמי', date: getDeadline({ startDate: pretrialAsDate,  before: 20}), },
+    pretrialAsDate && { name: 'רשימת בקשות', date: getDeadline({ startDate: pretrialAsDate,  before: 20}), },
     pretrialAsDate && { name: 'רשימת עדים (תובע)', date: getDeadline({ startDate: pretrialAsDate,  before: 20}), },
-    pretrialAsDate && { name: 'רשימת עדים', date: getDeadline({ startDate: pretrialAsDate,  before: 7}), },
+    pretrialAsDate && { name: 'רשימת עדים (נתבע)', date: getDeadline({ startDate: pretrialAsDate,  before: 7}), },
   ]
     .filter(Boolean)
     .sort((a, b) => {
       return a.date.valueOf() - b.date.valueOf();
     });
   
+  // TODO: Integrate with Google Calendar
   return (
     <div className={s9(c.tableWrapper)}>
       <table className={s9(c.table)}>
--- src/utils/getDeadline/getDeadline.spec.ts
+++ src/utils/getDeadline/getDeadline.spec.ts
@@ -14,17 +14,31 @@ test("return correct before deadline in a different month", () => {
 
   expect(result.toDateString()).toBe(expected.toDateString());
 });
-test("correctly adjust a before deadline when it falls on an off day and then a weekend", () => {
+test("correctly adjust a before deadline when it falls within a recess and then a sabatical", () => {
   const startDate = new Date("2023-04-13");
   const result = getDeadline({ startDate, before: 4 });
   const expected = new Date("2023-04-02");
 
   expect(result.toDateString()).toBe(expected.toDateString());
 });
-test("correctly adjust a before deadline when it falls on an off day", () => {
-  const startDate = new Date("2023-10-08");
+test("correctly adjust a before deadline when it falls within a recess", () => {
+  const startDate = new Date("2023-04-13");
   const result = getDeadline({ startDate, before: 4 });
-  const expected = new Date("2023-09-25");
+  const expected = new Date("2023-04-02");
+
+  expect(result.toDateString()).toBe(expected.toDateString());
+});
+test("add days when passing over a recess in a before deadline", () => {
+  const startDate = new Date("2023-04-13");
+  const result = getDeadline({ startDate, before: 10 });
+  const expected = new Date("2023-03-26");
+
+  expect(result.toDateString()).toBe(expected.toDateString());
+});
+test("add days when passing over two recesses in a before deadline", () => {
+  const startDate = new Date("2023-10-08");
+  const result = getDeadline({ startDate, before: 90 });
+  const expected = new Date("2023-05-15");
 
   expect(result.toDateString()).toBe(expected.toDateString());
 });
@@ -42,14 +56,28 @@ test("return correct after deadline in a different month", () => {
 
   expect(result.toDateString()).toBe(expected.toDateString());
 });
-test("return correct after deadline when it falls on a weekend", () => {
+test("return correct after deadline when it falls on a sabatical", () => {
   const startDate = new Date("2023-06-08");
   const result = getDeadline({ startDate, after: 1 });
   const expected = new Date("2023-06-11");
 
   expect(result.toDateString()).toBe(expected.toDateString());
 });
-test("correctly adjust an after deadline when it falls on an off day", () => {
+test("add days when passing over a recess in a after deadline", () => {
+  const startDate = new Date("2023-09-28");
+  const result = getDeadline({ startDate, after: 1 });
+  const expected = new Date("2023-10-08");
+
+  expect(result.toDateString()).toBe(expected.toDateString());
+});
+test("add days when passing over two recesses in a after deadline", () => {
+  const startDate = new Date("2023-07-21");
+  const result = getDeadline({ startDate, after: 90 });
+  const expected = new Date("2023-12-14");
+
+  expect(result.toDateString()).toBe(expected.toDateString());
+});
+test("correctly adjust an after deadline when it falls within a recess", () => {
   const startDate = new Date("2023-04-04");
   const result = getDeadline({ startDate, after: 5 });
   const expected = new Date("2023-04-17");
